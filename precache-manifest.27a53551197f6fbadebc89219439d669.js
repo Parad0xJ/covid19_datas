a1555c9c590584541ca2aaf77ed6d06a9ba479de
Parad0xJ <paradoxlyon@gmail.com>
2020-04-15 14:26:38 +0200

    Updates

---
+++ precache-manifest.27a53551197f6fbadebc89219439d669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e528fe752b470e7a0f8f00ac9967ab9",
+    "url": "/covid19_datas/index.html"
+  },
+  {
+    "revision": "8481e0dba30dea764ab1",
+    "url": "/covid19_datas/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "80338641c44f0bffd1ae",
+    "url": "/covid19_datas/static/css/main.ade2a842.chunk.css"
+  },
+  {
+    "revision": "8481e0dba30dea764ab1",
+    "url": "/covid19_datas/static/js/2.c3c1f1b5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19_datas/static/js/2.c3c1f1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80338641c44f0bffd1ae",
+    "url": "/covid19_datas/static/js/main.faf88609.chunk.js"
+  },
+  {
+    "revision": "42d48cafa016f57495ec",
+    "url": "/covid19_datas/static/js/runtime-main.169aa9c3.js"
+  }
+]);
