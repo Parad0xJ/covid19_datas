71f494a2aba6b59896be6acae7f8d56af22b5ea4
Parad0xJ <paradoxlyon@gmail.com>
2020-04-15 23:49:06 +0200

    remplacement cas par pers infectees dans resultat pays

--- src/PaysData.js
+++ src/PaysData.js
@@ -7,7 +7,7 @@ export const PaysData = props => {
             <div className="card-header countryName">{props.country}</div>
             <div className="card-body text-dark">
                 <p className="card-text">{`Personnes inféctées : `}<span className='paysDatas'><strong>{props.cas}</strong></span></p>
-                <p className="card-text">{`Aujourd'hui  : `}<span className='paysDatas'><strong> + {props.todayCases} cas</strong></span></p>
+                <p className="card-text">{`Aujourd'hui  : `}<span className='paysDatas'><strong> + {props.todayCases} </strong><small className='text-secondary'>pers. infectées</small></span></p>
                 <p className="card-text">{`Décés : `}<span className='paysDatas'><strong>{props.deaths}</strong></span> ||
                   <span className='ml-1'>{`Décés du jour : `}</span><span className='paysDatas'><strong>{props.todayDeaths}</strong></span> ||
                     <span className='ml-1'>{`Malade dans un état critique: `}</span><span className='paysDatas'><strong>{props.criticalCase}</strong></span></p>
